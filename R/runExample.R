5811c7ddce2db9a93fb2729d60165e1570f6efe9
migdal <maciej_migdal@o2.pl>
2018-08-18 13:12:57 +0200

    Add input file button

--- R/runExample.R
+++ R/runExample.R
@@ -1,5 +1,5 @@
 runExample <- function() {
-  appDir <- "/home/mmigdal/Documents/site_things/sumsamstats/inst/sumsamstats"
+  appDir <- "/home/mmigdal/Documents/sumsamstats/inst/sumsamstats" #it must find path by itself
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing 'sumsamstats'.", call. = FALSE)
   }
