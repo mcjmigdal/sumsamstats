d9cbbe91b727877315fe9b89bf0f888ce4507bd4
migdal <maciej_migdal@o2.pl>
2018-08-18 20:31:18 +0200

    Add description at main page and simplify input reading logic

--- inst/sumsamstats/server.R
+++ inst/sumsamstats/server.R
@@ -7,84 +7,53 @@ exampleInput = list.files("/home/mmigdal/Documents/sumsamstats/", ".log$", full.
 
 server <- function(input, output, session) {
 
+    data = reactive({
+        paths <- input$inputFiles$datapath
+        if (is.null(paths)) {
+            paths = exampleInput
+        }
+        lapply(paths, readSamtoolsStats)
+    })  # TODO SET UP DEFAULT PATHs TO EXEMPLARY LOGS
 
-
-  data = reactive({
-    paths <- input$inputFiles$datapath
-    if (is.null(paths)) {
-      paths = exampleInput
-    }
-    lapply(paths, readSamtoolsStats)
-  }) # TODO SET UP DEFAULT PATHs TO EXEMPLARY LOGS
-
-  samples = reactive({
-    names <- input$inputFiles$name
-    if (is.null(names)) {
-      names = exampleInput
-    }
-    gsub(".stats.log$", "", gsub(".*/", "", names))
-  })
-
-  summaryNumbers = reactive(do.call(rbind, lapply(1:length(data()), function(i) {
-    df = data()
-    df[[i]]$SN$sample = factor(samples()[i])
-    df[[i]]$SN
-  })) %>% mutate(value = as.numeric(value), description = gsub(":$", "", description)))
-
-  insertSize = reactive(do.call(rbind, lapply(1:length(data()), function(i) {
-    df = data()
-    df[[i]]$IS$sample = factor(samples()[i])
-    df[[i]]$IS
-  })) %>% mutate_if(is.character, as.numeric) %>% group_by(sample) %>% mutate(pairs_total = pairs_total/sum(pairs_total)))
-
-
-    output$summaryInputSamples <- renderUI(checkboxGroupInput(inputId = "samplesSummaryNumbers",
-                                  label = "Samples to show:",
-                                  choices = unique(summaryNumbers()$sample),
-                                  selected = unique(summaryNumbers()$sample)
-                                  )
-    )
-
-    output$summaryInputProperty <- renderUI(selectInput(inputId = "what",
-                                               label = "Property to plot:",
-                                               choices = unique(summaryNumbers()$description),
-                                               selected = "raw total sequences:"
-                                               )
-    )
-
-    output$summaryNumbersPlot <- renderPlot({
-      plotSummaryNumbers(data = summaryNumbers(),
-                         samples = input$samplesSummaryNumbers,
-                         what = input$what
-                         )
+    samples = reactive({
+        names <- input$inputFiles$name
+        if (is.null(names)) {
+            names = exampleInput
+        }
+        gsub(".*/", "", names)
     })
 
-    output$samplesInsertSize <- renderUI(checkboxGroupInput(inputId = "samplesInsertSize",
-                       label = "Samples to show:",
-                       choices = unique(summaryNumbers()$sample),
-                       selected = unique(summaryNumbers()$sample)
-                       )
-    )
+    summaryNumbers = reactive(do.call(rbind, lapply(1:length(data()), function(i) {
+        df = data()
+        df[[i]]$SN$sample = factor(samples()[i])
+        df[[i]]$SN
+    })) %>% mutate(value = as.numeric(value), description = gsub(":$", "", description)))
 
-    output$scaleInsertSize <- renderUI(selectInput(inputId = "scaleInsertSize",
-                label = "Scale:",
-                choices = c("normal", "log"),
-                selected = "normal"
-                )
-    )
+    insertSize = reactive(do.call(rbind, lapply(1:length(data()), function(i) {
+        df = data()
+        df[[i]]$IS$sample = factor(samples()[i])
+        df[[i]]$IS
+    })) %>% mutate_if(is.character, as.numeric) %>% group_by(sample) %>% mutate(pairs_total = pairs_total/sum(pairs_total)))
 
-    output$limsInsertSize <- renderUI(sliderInput(inputId = "limsInsertSize",
-                label = "Range:",
-                min = 0, max = 2000,
-                value = c(0,400)
-                )
-    )
+
+    output$summaryInputSamples <- renderUI(checkboxGroupInput(inputId = "samplesSummaryNumbers", label = "Samples to show:", choices = unique(summaryNumbers()$sample),
+        selected = unique(summaryNumbers()$sample)))
+
+    output$summaryInputProperty <- renderUI(selectInput(inputId = "what", label = "Property to plot:", choices = unique(summaryNumbers()$description),
+        selected = "raw total sequences:"))
+
+    output$summaryNumbersPlot <- renderPlot({
+        plotSummaryNumbers(data = summaryNumbers(), samples = input$samplesSummaryNumbers, what = input$what)
+    })
+
+    output$samplesInsertSize <- renderUI(checkboxGroupInput(inputId = "samplesInsertSize", label = "Samples to show:", choices = unique(summaryNumbers()$sample),
+        selected = unique(summaryNumbers()$sample)))
+
+    output$scaleInsertSize <- renderUI(selectInput(inputId = "scaleInsertSize", label = "Scale:", choices = c("normal", "log"), selected = "normal"))
+
+    output$limsInsertSize <- renderUI(sliderInput(inputId = "limsInsertSize", label = "Range:", min = 0, max = 2000, value = c(0, 400)))
 
     output$insertSizePlot <- renderPlot({
-        plotInsertSize(data = insertSize(),
-                       samples = input$samplesInsertSize,
-                       log = ifelse(input$scaleInsertSize == "log", T, F),
-                       lims = input$limsInsertSize
-                       )
+        plotInsertSize(data = insertSize(), samples = input$samplesInsertSize, log = ifelse(input$scaleInsertSize == "log", T, F), lims = input$limsInsertSize)
     })
 }
--- inst/sumsamstats/ui.R
+++ inst/sumsamstats/ui.R
@@ -1,9 +1,28 @@
-ui <- navbarPage(title="Summary of samtools stats output",
+ui <- navbarPage(title="ATAC-seq quality control",
 
 
-    tabPanel("Home",
-             h1("Summary of samtools stats output"),
-             fileInput(inputId = "inputFiles", label = "Input files", multiple = TRUE)
+    tabPanel("Input files",
+             h1("Samtools stats based ATAC-seq quality control"),
+             p("This app summarises output of samtools stats in a way that allows quick inspection of ATAC-seq libraries quality,
+               quality standards were taken from", a(href="https://www.encodeproject.org/atac-seq", "ENCODE website.")),
+             br(),
+             p("By default app preloads exemplary input files, so you might explore it! Just select any other tab above."),
+             p("If you prefer to check quality of your libraries provide outputs from samtools stats below."),
+             br(),
+             fileInput(inputId = "inputFiles", label = "Input files:", multiple = TRUE),
+             hr(),
+             h3("ATAC-seq quality standards:"),
+             tags$ul(
+               tags$li("two or more biological replicates"),
+               tags$li("at least 25 million fragments"),
+               tags$li("aligment rate greater than 95%"),
+               tags$li("replicate concordance measured by calculating IDR values"),
+               tags$li("library complexity measured using Non-Redundant Fraction and PCR Bottlenecking Coefficients 1 and 2. NRF > 0.9, PBC1 > 0.9, PBC2 > 3"),
+               tags$li("mononucleosome peak must be present in the fragment length distribution"),
+               tags$li("FRiP > 0.3"),
+               tags$li("TSS enrichment")
+             )
+
     ),
 
     tabPanel("Summary statistics",
