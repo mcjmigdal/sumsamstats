72c5749bb374bc28f78ccc03a55e46e44fd6fb0d
migdal migdal <mcjmigdal@gmail.com>
2018-08-28 11:12:13 +0200

    Write fitting function

--- R/fit.R
+++ R/fit.R
@@ -6,4 +6,7 @@ gaussFun <- function(x, a, mean, sd) {
     a * exp(-0.5 * ((x - mean)/sd)^2)
 }
 
-
+nlsATAC <- function(gnumber = 3) {
+  gaussians <- paste(sapply(1:gnumber, function(i) paste0("gaussFun(k", i, ", mean", i, ", sd", i, ")")), collapse = " + ")
+  fit <- as.formula(paste0("y ~ exponentFun(x, a, b) +", gaussians))
+}
