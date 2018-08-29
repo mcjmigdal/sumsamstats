70c8b12fac6a40cb59dad0098d15d2677fc2c25b
migdal migdal <mcjmigdal@gmail.com>
2018-08-29 11:30:19 +0200

    Add function for fitting mixture of gaussians and exponent

--- R/fit.R
+++ R/fit.R
@@ -6,7 +6,14 @@ gaussFun <- function(x, a, mean, sd) {
     a * exp(-0.5 * ((x - mean)/sd)^2)
 }
 
-nlsATAC <- function(gnumber = 3) {
-  gaussians <- paste(sapply(1:gnumber, function(i) paste0("gaussFun(k", i, ", mean", i, ", sd", i, ")")), collapse = " + ")
-  fit <- as.formula(paste0("y ~ exponentFun(x, a, b) +", gaussians))
+nlsATAC <- function(data, start, gnumber = 3, ...) {
+  gaussians <- paste(sapply(1:gnumber, function(i) paste0("gaussFun(insert_size, k", i, ", mean", i, ", sd", i, ")")), collapse = " + ")
+  fit_formula <- as.formula(paste0("pairs_total ~ exponentFun(insert_size, a, b) +", gaussians))
+  model <- nlsLM(
+    formula = fit_formula,
+    data = data,
+    start = start,
+    ...
+  )
+  return(model)
 }
